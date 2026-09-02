a4cb906553a5f72f63ad42ba37d039e99af5ea57
GamiKazu <onetree0407@icloud.com>
2026-09-02 17:23:08 +0900

    Implement mobile navigation toggle functionality

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Main script
+const menuButton = document.querySelector(".menu-button");
+const globalNav = document.querySelector(".global-nav");
+
+if (menuButton && globalNav) {
+  menuButton.addEventListener("click", () => {
+    const isOpen = globalNav.classList.toggle("is-open");
+    menuButton.setAttribute("aria-expanded", isOpen ? "true" : "false");
+  });
+
+  globalNav.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+      globalNav.classList.remove("is-open");
+      menuButton.setAttribute("aria-expanded", "false");
+    });
+  });
+}
